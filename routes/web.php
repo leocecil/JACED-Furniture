41b33f133a612b8ede64d290c36db5828f316b0c
diahlokamp <pdiahloka@student.ciputra.ac.id>
2026-05-11 17:58:45 +0700

    diah update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('store.checkout');
 });
+
+Route::get('/transactionhistory', function () {
+    return view('store.transaction_history');
+});
