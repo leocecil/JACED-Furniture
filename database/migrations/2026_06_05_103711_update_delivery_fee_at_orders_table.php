d192d58e3184e7188b852915a2913466c7ebc479
leocecil <ceciliaaleo08@gmail.com>
2026-06-05 10:39:42 +0700

    del fee

---
+++ database/migrations/2026_06_05_103711_update_delivery_fee_at_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('orders', function (Blueprint $table) {
+            //update delivery_fee
+            $table->decimal('delivery_fee', 12, 2)->change();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::table('orders', function (Blueprint $table) {
+            $table->decimal('delivery_fee', 10, 2)->change();
+        });
+    }
+};
